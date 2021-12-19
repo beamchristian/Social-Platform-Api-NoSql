ce144dfcbcb0900a16be400c86f69c126b79f29e
Christian Beam <beamchristian@yahoo.com>
2021-12-18 21:58:20 -0500

    CRUD for thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -27,6 +27,39 @@ const thoughtController = {
             res.status(400).json(err);
           });
       },
+
+      // createThought
+createThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update thought by id
+updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // delete thought
+deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
     }
 
 module.exports = thoughtController;
