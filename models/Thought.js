51592455d0a37ebb5b01ee2805634b4150d441bd
Christian Beam <beamchristian@yahoo.com>
2021-12-18 21:36:06 -0500

    Thought model and dateFormat util

---
+++ models/Thought.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true
+      },
+    reactions: [reactionSchema]
+  });
+
+  // create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
